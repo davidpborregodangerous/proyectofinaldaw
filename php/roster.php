63a6b159771280c6546f0122dbcdaf74ee66f94a
davidpborregodangerous <49269210+davidpborregodangerous@users.noreply.github.com>
2020-10-21 19:26:42 +0200

    Actualización FAT
    -Terminada la página de la sección Vídeos.
    -Videos de youtube añadidos con Iframes.
    -Efecto de sombra al hacer hover sobre el recuadro de: superestrella, noticia y vídeo.

--- php/roster.php
+++ php/roster.php
@@ -14,31 +14,31 @@
 			<!-- Noticias Recientes Carrusel -->
 			<h1 class="seccion2">ROSTER</h1>
 			<div id="roster">
-				<a href=""><img alt="Taker" src="../img/roster/Undertaker.png"></a>
-				<a href=""><img alt="Fiend" src="../img/roster/Fiend.png"></a>
-				<a href=""><img alt="Aleister" src="../img/roster/AleisterBlack.png"></a>
-				<a href=""><img alt="Kross" src="../img/roster/KarrionKross.png"></a>
-				<a href=""><img alt="Dexter" src="../img/roster/DexterLumis.png"></a>
+				<a href="https://espanol.wwe.com/superstars/undertaker"><img alt="Taker" src="../img/roster/Undertaker.png"></a>
+				<a href="https://www.wwe.com/superstars/the-fiend"><img alt="Fiend" src="../img/roster/Fiend.png"></a>
+				<a href="https://www.wwe.com/superstars/aleister-black"><img alt="Aleister" src="../img/roster/AleisterBlack.png"></a>
+				<a href="https://www.wwe.com/superstars/karrion-kross"><img alt="Kross" src="../img/roster/KarrionKross.png"></a>
+				<a href="https://www.wwe.com/superstars/dexter-lumis"><img alt="Dexter" src="../img/roster/DexterLumis.png"></a>
 				<a href=""><img alt="Wyatt" src="../img/roster/BrayWyatt.png"></a>
-				<a href=""><img alt="Bliss" src="../img/roster/AlexaBliss.png"></a>
-				<a href=""><img alt="Liv" src="../img/roster/LivMorgan.png"></a>
-				<a href=""><img alt="Becky" src="../img/roster/BeckyLynch.png"></a>
-				<a href=""><img alt="Sting" src="../img/roster/Sting.png"></a>
-				<a href=""><img alt="Hogan" src="../img/roster/HulkHogan.png"></a>
-				<a href=""><img alt="Warrior" src="../img/roster/UltimateWarrior.png"></a>
-				<a href=""><img alt="Andre" src="../img/roster/AndreTheGiant.png"></a>
-				<a href=""><img alt="Macho" src="../img/roster/RandySavage.png"></a>
-				<a href=""><img alt="StoneCold" src="../img/roster/SteveAustin.png"></a>
-				<a href=""><img alt="TheRock" src="../img/roster/Rock.png"></a>
-				<a href=""><img alt="Punk" src="../img/roster/CMPunk.png"></a>
-				<a href=""><img alt="Edge" src="../img/roster/Edge.png"></a>
-				<a href=""><img alt="Kane" src="../img/roster/Kane.png"></a>
-				<a href=""><img alt="Pronto" src="../img/roster/Soon.png"></a>
-				<a href=""><img alt="Pronto" src="../img/roster/Soon.png"></a>
-				<a href=""><img alt="Pronto" src="../img/roster/Soon.png"></a>
-				<a href=""><img alt="Pronto" src="../img/roster/Soon.png"></a>
-				<a href=""><img alt="Pronto" src="../img/roster/Soon.png"></a>
-				<a href=""><img alt="Pronto" src="../img/roster/Soon.png"></a>
+				<a href="https://www.wwe.com/superstars/alexa-bliss"><img alt="Bliss" src="../img/roster/AlexaBliss.png"></a>
+				<a href="https://www.wwe.com/superstars/liv-morgan"><img alt="Liv" src="../img/roster/LivMorgan.png"></a>
+				<a href="https://www.wwe.com/superstars/becky-lynch"><img alt="Becky" src="../img/roster/BeckyLynch.png"></a>
+				<a href="https://www.wwe.com/superstars/sting"><img alt="Sting" src="../img/roster/Sting.png"></a>
+				<a href="https://www.wwe.com/superstars/hulkhogan"><img alt="Hogan" src="../img/roster/HulkHogan.png"></a>
+				<a href="https://www.wwe.com/superstars/ultimatewarrior"><img alt="Warrior" src="../img/roster/UltimateWarrior.png"></a>
+				<a href="https://www.wwe.com/superstars/andrethegiant"><img alt="Andre" src="../img/roster/AndreTheGiant.png"></a>
+				<a href="https://www.wwe.com/superstars/randysavage"><img alt="Macho" src="../img/roster/RandySavage.png"></a>
+				<a href="https://www.wwe.com/superstars/stone-cold-steve-austin"><img alt="StoneCold" src="../img/roster/SteveAustin.png"></a>
+				<a href="https://www.wwe.com/superstars/the-rock"><img alt="TheRock" src="../img/roster/Rock.png"></a>
+				<a href="https://www.wwe.com/superstars/cmpunk"><img alt="Punk" src="../img/roster/CMPunk.png"></a>
+				<a href="https://www.wwe.com/superstars/edge"><img alt="Edge" src="../img/roster/Edge.png"></a>
+				<a href="https://www.wwe.com/superstars/kane"><img alt="Kane" src="../img/roster/Kane.png"></a>
+				<img alt="Pronto" src="../img/roster/Soon.png">
+				<img alt="Pronto" src="../img/roster/Soon.png">
+				<img alt="Pronto" src="../img/roster/Soon.png">
+				<img alt="Pronto" src="../img/roster/Soon.png">
+				<img alt="Pronto" src="../img/roster/Soon.png">
+				<img alt="Pronto" src="../img/roster/Soon.png">
 			</div>
 			<h1 class="seccion2">Y MÁS PRONTO</h1>
 			<?php
--- php/videos.php
+++ php/videos.php
@@ -12,8 +12,32 @@
 				include '../html/header.html';
 			?>
 			<!-- Videos Recientes Carrusel -->
-			<h1 id="seccion">Ultimos Vídeos</h1>
+			<h1 class="seccion2">ÚLTIMOS VÍDEOS</h1>
 			<div id="videos">
+				<div class="gallery">
+					<iframe src="https://www.youtube.com/embed/eh5BZR-Y6cA"></iframe>
+					<div class="desc">
+						<p>WWE 2KBATTLEGROUNDS TRAILER</p>
+					</div>
+				</div>
+				<div class="gallery">
+					<iframe src="https://www.youtube.com/embed/oObN-pSBbq8"></iframe>
+					<div class="desc">
+						<p>WWE 2K20 ORIGINALS: EMPIRE OF TOMORROW TRAILER</p>
+					</div>
+				</div>
+				<div class="gallery">
+					<iframe src="https://www.youtube.com/embed/E4bjLKulEno"></iframe>
+					<div class="desc">
+						<p>WWE 2K20 ORIGINALS: SOUTHPAW REGIONAL WRESTLING TRAILER</p>
+					</div>
+				</div>
+				<div class="gallery">
+					<iframe src="https://www.youtube.com/embed/eRF8X3qg0Xk"></iframe>
+					<div class="desc">
+						<p>WWE 2K20 MYCAREER TRAILER</p>
+					</div>
+				</div>
 			</div>
 			<?php
 				include '../html/footer.html';
